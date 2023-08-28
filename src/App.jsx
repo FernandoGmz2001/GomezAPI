2211e425f66187b43a1b21a40af899f316326565
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-08-28 08:01:57 -0500

    feat(PeticionParametros):Rename folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import FetchParameters from "./Parcial 1/PeticionesConParametros/FetchParameters";
+import FetchParameters from "./Parcial 1/PeticionParametros/FetchParameters";
 import FetchApi from "./Parcial 1/PeticionesJs/FetchApi";
 
 function App() {
