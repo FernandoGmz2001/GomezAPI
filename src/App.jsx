ca298048dcbc8bf67c2066fac55471710a2329b3
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-08-30 01:28:46 -0500

    fix(App): Fixed component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import CustomApp from "./Parcial 1/CustomModal/CustomApp";
-import FetchParameters from "./Parcial 1/PeticionParametros/FetchParameters";
+import CustomApp from "./Parcial 1/EcmaModule/CustomApp";
 // import FetchApi from "./Parcial 1/PeticionesJs/FetchApi";
 
 function App() {
